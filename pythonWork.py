27fbe48c99d743255652d4ce95a5614f3f80d605
E-N-House <116841519+E-N-House@users.noreply.github.com>
2023-07-04 14:11:37 -0400

    added prime checker

--- pythonWork.py
+++ pythonWork.py
@@ -1,4 +1,5 @@
 import math
+# paint cans needed excercise
 def paint_calculations(height, width, coverage):
   result = math.ceil((height*width)/cover)
   print(f"You'll need {result} cans of paint")
@@ -9,3 +10,17 @@ coverage_per_can = 5
   
 paint_calculations(height=height_input, width=width_input, cover=coverage_per_can)
                    
+# finding prime numbers excercise
+def prime_checker(number):
+    edge_case_primes = [2, 3, 5, 7]
+    if number == 1:
+        print("It's not a prime number")
+    elif number in edge_case_primes:
+        print("It's a prime number.")
+    elif number%2 == 0 or number%3==0 or number%5==0 or number%7==0:
+        print("It's not a prime number")
+    else:
+        print("It's a prime number")
+      
+input_number = int(input("Enter number to check: "))
+prime_checker(number=input_number)
