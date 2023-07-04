2b54566446771bed522b79efc80e949b79dab81d
E-N-House <116841519+E-N-House@users.noreply.github.com>
2023-07-04 13:40:56 -0400

    Create pythonWork.py

---
+++ pythonWork.py
@@ -0,0 +1,10 @@
+def paint_calculations(height, width, coverage):
+  result = round((height*width)/cover)
+  print(f"You'll need {result} cans of paint")
+height_input = int(input("Height of Wall: "))
+width_input = int(input("Width of Wall: "))
+coverage_per_can = 5
+  
+  
+paint_calculations(height=height_input, width=width_input, cover=coverage_per_can)
+                   
