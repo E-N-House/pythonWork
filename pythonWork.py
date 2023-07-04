5bd8303a5a58ef84393c16d5437a22d44a5fb092
E-N-House <116841519+E-N-House@users.noreply.github.com>
2023-07-04 14:49:32 -0400

    2nd version of prime checker

--- pythonWork.py
+++ pythonWork.py
@@ -10,8 +10,8 @@ coverage_per_can = 5
   
 paint_calculations(height=height_input, width=width_input, cover=coverage_per_can)
                    
-# finding prime numbers excercise
-def prime_checker(number):
+# finding prime numbers excercise first version manually typing in
+def prime_checker_v1(number):
     edge_case_primes = [2, 3, 5, 7]
     if number == 1:
         print("It's not a prime number")
@@ -22,5 +22,25 @@ def prime_checker(number):
     else:
         print("It's a prime number")
       
+# finding prime using loop over prime divisables in edge prime cases
+def prime_checker_v2(number):
+    edge_case_primes = [2, 3, 5, 7]
+    isPrime = True
+    if number == 1:
+        print("It's not a prime number.")
+    elif number in edge_case_primes:
+        print("It's a prime number.")
+    else:
+        for i in edge_case_primes:
+            if number % i == 0:
+                isPrime = False
+        if isPrime:
+            print("It's a prime number.")
+        elif isPrime == False:
+            print("It's not a prime number.")
+
+
+
+  
 input_number = int(input("Enter number to check: "))
 prime_checker(number=input_number)
