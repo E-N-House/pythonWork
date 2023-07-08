c61d0619267314c2e3ef2c57fcaa098afc9add7f
E-N-House <116841519+E-N-House@users.noreply.github.com>
2023-07-08 13:11:57 -0400

    Update functionNotes.py
    function notes

--- functionNotes.py
+++ functionNotes.py
@@ -1,1 +1,14 @@
+# keyword is def my_func(var): then indented code block
+def my_function():
+  print("hello")
+  print("bye")
 
+# can place inputs within the () very similiar to js
+def format_name(f_name, l_name):
+  return f"{f_name.title()} {l_name.title()}"
+
+name = format_name("hhhhh", "bye")
+def greeting(full_name):
+  print(f"Hello {name} welcome to the show")
+
+greeting(name)
