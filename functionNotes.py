32b8edb60ffe4629e4e0735db9f96ae21dbd8ddc
E-N-House <116841519+E-N-House@users.noreply.github.com>
2023-07-08 13:12:34 -0400

    Update functionNotes.py
    Find days of month excercise

--- functionNotes.py
+++ functionNotes.py
@@ -12,3 +12,31 @@ def greeting(full_name):
   print(f"Hello {name} welcome to the show")
 
 greeting(name)
+
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+        return True
+  else:
+     return False
+
+def days_in_month(input_year, input_month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+  if input_month == 2:
+      if is_leap(input_year):
+          return 29
+      else:
+          return 28
+  else:
+      return month_days[input_month - 1]
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
