8c59a6daf93d54f1f0b7fee8af6e59c15a2e4fdc
E-N-House <116841519+E-N-House@users.noreply.github.com>
2023-07-08 13:48:55 -0400

    Create calculator.py
    Also have + - * and / as defined functions

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# Calculator functions
+def add(n1, n2):
+  """ Takes two numbers and adds them together """
+  return n1 + n2
+
+def subtract(n1, n2):
+  """ Takes two numbers and subtracts number 1 from number 2 """
+  return n1 - n2
+
+def multiply(n1, n2):
+  """ Takes two numbers and multiplies together """
+  return n1 * n2
+
+def divide(n1, n2):
+  """ Takes two numbers and divides number 1 by number 2 """
+  return n1 / n2
+
