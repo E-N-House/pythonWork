d295451679b471e9d8e4b2d1c94e2285d7f227bf
E-N-House <116841519+E-N-House@users.noreply.github.com>
2023-07-08 13:22:13 -0400

    Update functionNotes.py
    Error handling for invalid month.
    Cleaned up logic in if month == 2

--- functionNotes.py
+++ functionNotes.py
@@ -26,12 +26,11 @@ def is_leap(year):
      return False
 
 def days_in_month(input_year, input_month):
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
-  if input_month == 2:
-      if is_leap(input_year):
+  if input_month > 12 or input_month < 1:
+      return "Invalid month"
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+  if input_month == 2 and is_leap(input_year):
           return 29
-      else:
-          return 28
   else:
       return month_days[input_month - 1]
   
